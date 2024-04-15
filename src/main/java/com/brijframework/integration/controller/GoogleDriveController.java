8e67ae533f39165ac3001116792ef23c3ff16e94
Ram Kishor <krishnaenterprises0423@gmail.com>
2024-04-15 10:34:02 +0530

    ok

--- src/main/java/com/brijframework/integration/controller/GoogleDriveController.java
+++ src/main/java/com/brijframework/integration/controller/GoogleDriveController.java
@@ -11,21 +11,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.brijframework.integration.service.GoogleDriveService;
+
 @RestController
 public class GoogleDriveController {
 
     @Autowired
-    AudioBookController audioBookController;
+    GoogleDriveService googleDriveService;
 
 
     @GetMapping("/")
     public  String sample() throws IOException, GeneralSecurityException{
-        return audioBookController.getAllAudio();
+        return googleDriveService.getAllAudio();
     }
 
     @PostMapping("/newaudio")
     @CrossOrigin(origins = "http://127.0.0.1:5173/")
     public  String upload(@RequestParam("audio") MultipartFile file) throws IOException, GeneralSecurityException{
-        return audioBookController.uploadAudio(file);
+        return googleDriveService.
     }
 }
